6b08faa7194f3eb6d380af4d256229aca0b064db
Typhoon <tayfunaliosman@gmail.com>
2024-09-17 14:01:43 +0300

    crawler log folder

---
+++ data-structures-and-algorithms/05 - Stacks & Queues/stacks-problems/easy-crawler-log-folder.py
@@ -0,0 +1,96 @@
+"""
+https://leetcode.com/problems/crawler-log-folder/
+
+The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+The operations are described below:
+
+"../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+"./" : Remain in the same folder.
+"x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
+The file system starts in the main folder, then the operations in logs are performed.
+
+Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+
+
+
+Example 1:
+
+
+
+Input: logs = ["d1/","d2/","../","d21/","./"]
+Output: 2
+Explanation: Use this change folder operation "../" 2 times and go back to the main folder.
+Example 2:
+
+
+
+Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
+Output: 3
+Example 3:
+
+Input: logs = ["d1/","../","../","../"]
+Output: 0
+
+
+Constraints:
+
+1 <= logs.length <= 103
+2 <= logs[i].length <= 10
+logs[i] contains lowercase English letters, digits, '.', and '/'.
+logs[i] follows the format described in the statement.
+Folder names consist of lowercase English letters and digits.
+
+
+"""
+
+
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        My solution, using stacks
+        TC = O(n)
+        SC = O(n)
+        :type logs: List[str]
+        :rtype: int
+        """
+        move_to_parent = "../"
+        remain_in_same_folder = "./"
+        stack = []
+        for operation in logs:
+            if operation == move_to_parent and stack:
+                stack.pop()
+            elif operation == remain_in_same_folder or operation == move_to_parent:
+                continue
+            else:
+                stack.append(operation)
+        print(stack)
+        return len(stack)
+
+    def minOperations(self, logs):
+        """
+        Using count
+        TC = O(n)
+        SC = O(1)
+        :type logs: List[str]
+        :rtype: int
+        """
+        count = 0
+        for operation in logs:
+            if operation == "../":
+                if count > 0:
+                    count -= 1
+            elif operation != "./":
+                count += 1
+        return count
+
+
+# logs = ["d1/","d2/","../","d21/","./"]
+# expected = 2
+logs = ["./", "../", "./"]
+expected = 0
+sol = Solution()
+print(sol.minOperations(logs))
+print(f"{expected=}")
