4b7df80239428e674d1b77641a88a4793eb51d05
Typhoon <tayfunaliosman@gmail.com>
2024-09-26 17:22:56 +0300

    Ransom note

--- data-structures-and-algorithms/07 - Hash Tables/problems/easy-ransom-note.py
+++ data-structures-and-algorithms/07 - Hash Tables/problems/easy-ransom-note.py
@@ -25,13 +25,56 @@ Constraints:
 1 <= ransomNote.length, magazine.length <= 105
 ransomNote and magazine consist of lowercase English letters.
 """
+import collections
 
 
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
         """
+        My initial code
+        SC = O(n) -> len ransomNote
+        TC = O( n + m)
         :type ransomNote: str
         :type magazine: str
         :rtype: bool
         """
-        pass
+        ransom_note_char_counts = collections.Counter(ransomNote)
+        for char in magazine:
+            if char in ransom_note_char_counts:
+                ransom_note_char_counts[char] -= 1
+        for char_count in ransom_note_char_counts.values():
+            if char_count > 0:
+                return False
+        return True
+
+
+    def canConstruct(self, ransomNote, magazine):
+        """
+
+        My solution, Optmized with early exits; we del all chars that are at 0,
+        we check ransom not on each del
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        if not ransomNote:
+            return True
+        if not magazine:
+            return False
+
+        ransom_note_char_counts = collections.Counter(ransomNote)
+        for char in magazine:
+            if char in ransom_note_char_counts:
+                ransom_note_char_counts[char] -= 1
+                if ransom_note_char_counts[char] == 0:
+                    del ransom_note_char_counts[char]
+                    if not ransom_note_char_counts:
+                        return True
+        return False
+
+ransomNote = "a"; magazine = "b"
+expected = False
+sol = Solution()
+res = sol.canConstruct(ransomNote, magazine)
+print(res)
+print(f"{expected=}")
